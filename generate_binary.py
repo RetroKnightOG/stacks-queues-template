0998edc172b4c8c6461bbb7270785147b0ad3536
RetroKnightOG <98424436+RetroKnightOG@users.noreply.github.com>
2022-02-27 20:03:50 -0500

    Update generate_binary.py

--- generate_binary.py
+++ generate_binary.py
@@ -1,25 +1,38 @@
 # MODIFY ME TO IMPLEMENT YOUR SOLUTION
 # TO PROBLEM 3: GENERATE BINARY NUMBER STRINGS
 #
-# NAME:         FIXME
+# NAME:         Omar Nasser
 # ASSIGNMENT:   Technical HW: Stacks & Queues
 
 from Queue import Queue
 
-# generate & return a queue of binary number strings from 1 to N
-# front of queue begins @ '1', returns empty Queue otherwise
-def generate_binary_numbers(N):
+
+
+def generate_binary_numbers(n):
     numbers = Queue([])
+    temp_q = Queue([])
+
+    temp_q.enq("1")
+
+    while n > 0:
+    
+        temp_q.enq(temp_q.front()+"0")
+        temp_q.enq(temp_q.front()+"1")
+    
+        numbers.enq(temp_q.deq())
+        n -= 1
 
     return numbers
 
 def main():
     generate_binary_numbers(2).print()
+    
     generate_binary_numbers(3).print()
+    
     generate_binary_numbers(6).print()
 
 
 # Don't run main on import
 if __name__ == "__main__":
+    
     main()
-
